d8dfc84156c24a35899c533783ca55718e6b5687
Bidyut316 <bidyutmandal316@gmail.com>
2024-04-09 16:25:52 +0530

    Done

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
-import React from 'react';
-import {View, ActivityIndicator, Text} from 'react-native';
+import React,{useEffect} from 'react';
+import {View, ActivityIndicator, Text,Alert } from 'react-native';
 import {SplashScreen} from './src/screens/splash.screens';
 import {LoginScreen} from './src/screens/login.screen';
 
@@ -15,7 +15,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-
+import messaging from '@react-native-firebase/messaging';
+// import { initializeApp } from '@react-native-firebase/app'; 
 import {AuthenticationContextProvider} from './src/services/auth/authcontext';
 import {RootStackScreen} from './src/navigation/account.navigator';
 
@@ -36,7 +37,41 @@ if (!firebase.apps.length) {
 
 
 export default function App() {
- 
+
+  useEffect(() => {
+    // Ensure Firebase is initialized before requesting permission
+  //  const app = initializeApp(firebaseConfig);
+  //  if (!firebase.apps.length) {
+  //   firebase.initializeApp(firebaseConfig);
+  // }
+    // Request permission for notifications
+    // async function requestUserPermission() {
+    //   try {
+    //     const authStatus = await messaging().requestPermission();
+    //     const enabled =
+    //       authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    //       authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    //     if (enabled) {
+    //       console.log('Authorization status:', authStatus);
+    //       const token= await messaging().getToken();
+    //       console.log("Token: ",token);
+          
+    //     }
+    //   } catch (error) {
+    //     console.error('Error requesting permission:', error);
+    //   }
+    // }
+    // requestUserPermission();
+
+    // const unsubscribe = messaging().onMessage(async remoteMessage => {
+    //   Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    // });
+
+    // return unsubscribe;
+  }, []);
+
+  // msg token:  dthptouLT6CfJR1jEmvx6p:APA91bFSXmGsPHAVsy94P1XjO3JWiZ3yndSkZQI_boePmgmn5zYh9GYuoXYDLvY5iyN-I2pL8IdcLofI4zlyZl97e6flu0TUULyzkAhE1WrErTZ7hjPNPUqxeqDkuphQxcph1nL7bagy
   return (
     <AuthenticationContextProvider>
       <NavigationContainer>
